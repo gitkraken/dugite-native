cb576a66656ffa18c895c3fc54290a77f37a5318
John Alden <john.alden@gitkraken.com>
2023-05-08 12:36:54 -0700

    update configure flags to match 7.61 defaults

--- script/build-ubuntu.sh
+++ script/build-ubuntu.sh
@@ -81,7 +81,12 @@ tar -xf $CURL_FILE
 
 (
 cd $CURL_FILE_NAME || exit 1
-./configure --prefix="$CURL_INSTALL_DIR" "$HOST" "$TARGET"
+./configure \
+  --enable-threaded-resolver \
+  --enable-tls-srp \
+  --prefix="$CURL_INSTALL_DIR" \
+  --with-ssl \
+  "$HOST" "$TARGET"
 make install
 )
 echo " -- Building git at $SOURCE to $DESTINATION"
